d6b1dfdc48706b23d5fac600b2d8f5630dfd9623
cit1566 <cit1566@gmailc.om>
2026-08-30 15:32:08 +0900

    [error] vite liveserver 변경 이력 인식 오류 - 문제를 해결하지 못해 다른 브렌치에서 작동 가능성 확인

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     tsconfigPaths: true,
   },
+  // 👇 이 부분을 추가하세요
   server: {
     watch: {
       usePolling: true,
